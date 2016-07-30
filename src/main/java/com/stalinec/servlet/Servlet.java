dec334bac329efaa94d0096dc13c8c11117a9e54
Yurii Tereshchuk <stalinec7@gmail.com>
2016-07-30 11:21:13 +0300

    Initial commit

---
+++ src/main/java/com/stalinec/servlet/Servlet.java
@@ -0,0 +1,27 @@
+package com.stalinec.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by mr.Smith on 30.07.2016.
+ */
+public class Servlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        process(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        process(request, response);
+    }
+
+    private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("index.jsp").forward(request, response);
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: mr.Smith
+  Date: 29.07.2016
+  Time: 23:14
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+    <h1>My servlet works!!!</h1>
+  </body>
+</html>
